2352445c257967b5fd29781ef49cd385998daf78
George Thomas Mohan <george.mohan@squareresults.com>
2025-04-15 16:09:15 -0700

    8

--- src/app/ContactSection.jsx
+++ src/app/ContactSection.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import Image from 'next/image';
 import { Montserrat } from 'next/font/google'
 import styles from './contact.module.css'
+import FadeInOnScroll from './FadeInOnScroll';
 import clientData from './clientData.json';
 
 const montserrat = Montserrat({
@@ -10,7 +11,7 @@ const montserrat = Montserrat({
     subsets: ['latin']
   })
 
-const PORTFOLIO_CONTACT_EMAIL = ''
+const PORTFOLIO_CONTACT_EMAIL = `dianakboykin@gmail.com`
 
 const ContactSection = () => {
     const [formData, setFormData] = useState({
@@ -26,7 +27,7 @@ const ContactSection = () => {
       const handleSubmit = async (e) => {
         e.preventDefault();
         // test mailto
-        let mailToString = `mailto:${clientData.contact.email}?cc=${formData.email}&subject=${formData.name} - Portfolio Contact Form&body=${formData.comment}`
+        let mailToString = `mailto:${PORTFOLIO_CONTACT_EMAIL}?cc=${formData.email}&subject=${formData.name} - Portfolio Contact Form&body=${formData.comment}`
         window.location = mailToString;
     
         // Send form data to your backend API
@@ -49,6 +50,7 @@ const ContactSection = () => {
 
     return (
         <div>
+             <FadeInOnScroll>
             <div className='section_title'>Contact</div>
             <div className={montserrat.className}>
                 <div className={styles.contact}>
@@ -81,6 +83,7 @@ const ContactSection = () => {
                     </form>
                 </div>
             </div>
+            </FadeInOnScroll>
         </div>
      
     );
